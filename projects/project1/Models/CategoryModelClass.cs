ca868e0fd144cbdbd827c624a0ffb24d234698a8
vadym <vadym582@revature.net>
2022-05-16 06:24:28 +0300

    Add Category display

--- projects/project1/Models/CategoryModelClass.cs
+++ projects/project1/Models/CategoryModelClass.cs
@@ -2,11 +2,8 @@ namespace Models
 {
 	public class CategoryModelClass
 	{
-		public class CategoryModelClass
-		{
-			public int CategoryId { get; set; }
-			public string Name { get; set; }
-			public string Description { get; set; }
-		}
+		public int CategoryId { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
 	}
 }
--- projects/project1/UI/UIClass.cs
+++ projects/project1/UI/UIClass.cs
@@ -204,7 +204,7 @@ public class UIClass
 	{
 		string userInput = "";
 
-		Console.WriteLine("Menu:\n1 - Cart\n2 - Log\n3 - Stores\n4 - Orders\n5 - Products\n6 - Categories\nq - Leave the store\n");
+		Console.WriteLine("Main menu:\n1 - Cart\n2 - Log\n3 - Stores\n4 - Orders\n5 - Products\n6 - Categories\nq - Leave the store\n");
 		userInput = Console.ReadLine();
 
 		switch (userInput)
@@ -269,7 +269,7 @@ public class UIClass
 		{
 			foreach (var item in listOfCategory)
 			{
-				//Console.WriteLine($"#{item.categoryId} located in:  {item.Location}");
+				Console.WriteLine($"Category #{item.CategoryId}\nName: {item.Name}\nDescription: {item.Description}\n");
 			}
 			Console.WriteLine();
 		}
