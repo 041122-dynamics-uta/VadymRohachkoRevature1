bd40952c18cee2f9c955b8fb8386c80cfc0a3fa5
vadym <vadym582@revature.net>
2022-07-08 02:14:46 +0300

    Moved methods out of Main

--- projects/ATM/Program.cs
+++ projects/ATM/Program.cs
@@ -69,46 +69,46 @@ public class cardHolder
 		_balance = newBalance;
 	}
 
+	void printOptions()
+	{
+		Console.WriteLine("Please choose from one of the following options...");
+		System.Console.WriteLine("1. Deposit");
+		System.Console.WriteLine("2. Withdraw");
+		System.Console.WriteLine("3. Show Balance");
+		System.Console.WriteLine("4. Exit");
+	}
+
+	public void deposit(cardHolder currentUser)
+	{
+		System.Console.WriteLine("How much $$ would you like to deposit?");
+		double deposit = Double.Parse(Console.ReadLine());
+		currentUser.setBalance(currentUser.getBalance() + deposit);
+		Console.WriteLine("Thank you for your $$. Your new balance is: " + currentUser.getBalance());
+	}
+
+	public void withdraw(cardHolder currentUser)
+	{
+		System.Console.WriteLine("How much $$ would you like to withdraw: ");
+		double withdrawal = Double.Parse(Console.ReadLine());
+		//check if the user has enough money
+		if (currentUser.getBalance() < withdrawal)
+		{
+			System.Console.WriteLine("Insufficient balance :(");
+		}
+		else
+		{
+			currentUser.setBalance(currentUser.getBalance() - withdrawal);
+			Console.WriteLine("$$ was withdrawn. Your new balance is: " + currentUser.getBalance());
+		}
+	}
+
+	public void balance(cardHolder currentUser)
+	{
+		System.Console.WriteLine("Current balance: " + currentUser.getBalance());
+	}
+
 	public static void Main(String[] args)
 	{
-		void printOptions()
-		{
-			Console.WriteLine("Please choose from one of the following options...");
-			System.Console.WriteLine("1. Deposit");
-			System.Console.WriteLine("2. Withdraw");
-			System.Console.WriteLine("3. Show Balance");
-			System.Console.WriteLine("4. Exit");
-		}
-
-		void deposit(cardHolder currentUser)
-		{
-			System.Console.WriteLine("How much $$ would you like to deposit?");
-			double deposit = Double.Parse(Console.ReadLine());
-			currentUser.setBalance(currentUser.getBalance() + deposit);
-			Console.WriteLine("Thank you for your $$. Your new balance is: " + currentUser.getBalance());
-		}
-
-		void withdraw(cardHolder currentUser)
-		{
-			System.Console.WriteLine("How much $$ would you like to withdraw: ");
-			double withdrawal = Double.Parse(Console.ReadLine());
-			//check if the user has enough money
-			if (currentUser.getBalance() < withdrawal)
-			{
-				System.Console.WriteLine("Insufficient balance :(");
-			}
-			else
-			{
-				currentUser.setBalance(currentUser.getBalance() - withdrawal);
-				Console.WriteLine("$$ was withdrawn. Your new balance is: " + currentUser.getBalance());
-			}
-		}
-
-		void balance(cardHolder currentUser)
-		{
-			System.Console.WriteLine("Current balance: " + currentUser.getBalance());
-		}
-
 		List<cardHolder> cardHolders = new List<cardHolder>();
 		cardHolders.Add(new cardHolder("1111222233334444", 1111, "John", "Brown", 150.22));
 		cardHolders.Add(new cardHolder("2222111133334444", 2222, "Amy", "Whitehouse", 180.02));
@@ -171,7 +171,7 @@ public class cardHolder
 		int option = 0;
 		do
 		{
-			printOptions();
+			currentUser.printOptions();
 			try
 			{
 				option = int.Parse(Console.ReadLine());
@@ -182,15 +182,15 @@ public class cardHolder
 			}
 			if (option == 1)
 			{
-				deposit(currentUser);
+				currentUser.deposit(currentUser);
 			}
 			else if (option == 2)
 			{
-				withdraw(currentUser);
+				currentUser.withdraw(currentUser);
 			}
 			else if (option == 3)
 			{
-				balance(currentUser);
+				currentUser.balance(currentUser);
 			}
 			else if (option == 4)
 			{
@@ -199,8 +199,6 @@ public class cardHolder
 			else { option = 0; }
 		} while (option != 4);
 		System.Console.WriteLine("Thank you! Good buy!");
-
-
 	}//end main
 }
 
